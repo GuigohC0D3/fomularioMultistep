3f5e179ede6825533cfaf6083061f0502320e307
GuigohC0D3 <guilhermewerneckpereira11@hotmail.com>
2024-07-01 11:50:32 -0300

    Fiz a estrutura do componente principal

--- form_multistep/src/App.jsx
+++ form_multistep/src/App.jsx
@@ -1,13 +1,37 @@
+// Components
+import { SlArrowLeft } from "react-icons/sl";
+import { SlArrowRight } from "react-icons/sl";
 
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className="App">
-      <h1>React MAP</h1>
+    <div className="app">
+      <div className="header">
+        <h2>Deixe sua avaliação</h2>
+        <p>
+          Ficamos felizes com a sua compra, utilize o formulário abaixo par
+          avaliar o produto
+        </p>
+      </div>
+      <div className="form-container">
+        <p>etapas</p>
+        <form>
+          <div className="inputs-container"></div>
+          <div className="actions">
+            <button type="button">
+              <SlArrowLeft />
+              <span>Voltar</span>
+            </button>
+            <button type="submit">
+              <span>Avançar</span>
+              <SlArrowRight />
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
